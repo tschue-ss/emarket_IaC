144d537e753292df25b9d8a924009292ca455162
tschue-ss <tschuess1324@naver.com>
2023-09-14 05:13:29 +0000

    update prefix on provider.tf terraform.tfvars

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "<<YOUR_S3_BUCKET_NAME>>"        # my-s3-bucket
+    bucket = "bucket-tschuess-230913"        # my-s3-bucket
     key    = "emarket/terraform.tfstate"
-    region = "<<YOUR_S3_BUCKET_REGION>>"      # us-east-1, us-west-2 
+    region = "us-east-1"      # us-east-1, us-west-2 
   }
   required_version = ">=1.1.3"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-aws_region      = "<<YOUR_AWS_REGION>>"           # us-east-1, us-west-2
-resource_prefix = "<<YOUR_RESOURCE_PREFIX>>"      # yang
+aws_region      = "us-east-1"           # us-east-1, us-west-2
+resource_prefix = "tschuess"      # yang
